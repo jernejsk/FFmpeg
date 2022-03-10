e163e6b2cfd7c974f2ae659ce0c84ecb788684fb
Lynne <dev@lynne.ee>
2022-03-10 18:03:05 +0100

    avcodec: add AVHWAccel.free_frame_priv callback

--- libavcodec/av1dec.c
+++ libavcodec/av1dec.c
@@ -25,6 +25,7 @@
 #include "libavutil/opt.h"
 #include "avcodec.h"
 #include "av1_parse.h"
+#include "decode.h"
 #include "av1dec.h"
 #include "bytestream.h"
 #include "codec_internal.h"
@@ -832,8 +833,7 @@ static int av1_frame_alloc(AVCodecContext *avctx, AV1Frame *f)
     if (avctx->hwaccel) {
         const AVHWAccel *hwaccel = avctx->hwaccel;
         if (hwaccel->frame_priv_data_size) {
-            f->hwaccel_priv_buf =
-                av_buffer_allocz(hwaccel->frame_priv_data_size);
+            f->hwaccel_priv_buf = ff_hwaccel_frame_priv_alloc(avctx, hwaccel);
             if (!f->hwaccel_priv_buf) {
                 ret = AVERROR(ENOMEM);
                 goto fail;
