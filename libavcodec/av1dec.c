6b67aa32fc3b9cad3e2487875e058ba726e0513d
Jonas Karlman <jonas@kwiboo.se>
2024-07-06 08:41:30 +0000

    avcodec: Add V4L2 Request API av1 hwaccel
    Add a V4L2 Request API hwaccel for AV1.
    
    Support for AV1 is enabled when Linux kernel headers declare the
    control id V4L2_CID_STATELESS_AV1_FRAME, added in v6.5.
    
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- libavcodec/av1dec.c
+++ libavcodec/av1dec.c
@@ -542,6 +542,7 @@ static int get_pixel_format(AVCodecContext *avctx)
                      CONFIG_AV1_D3D11VA_HWACCEL * 2 + \
                      CONFIG_AV1_D3D12VA_HWACCEL + \
                      CONFIG_AV1_NVDEC_HWACCEL + \
+                     CONFIG_AV1_V4L2REQUEST_HWACCEL + \
                      CONFIG_AV1_VAAPI_HWACCEL + \
                      CONFIG_AV1_VDPAU_HWACCEL + \
                      CONFIG_AV1_VIDEOTOOLBOX_HWACCEL + \
@@ -578,6 +579,9 @@ static int get_pixel_format(AVCodecContext *avctx)
 #if CONFIG_AV1_VULKAN_HWACCEL
         *fmtp++ = AV_PIX_FMT_VULKAN;
 #endif
+#if CONFIG_AV1_V4L2REQUEST_HWACCEL
+        *fmtp++ = AV_PIX_FMT_DRM_PRIME;
+#endif
         break;
     case AV_PIX_FMT_YUV420P10:
 #if CONFIG_AV1_DXVA2_HWACCEL
@@ -604,6 +608,9 @@ static int get_pixel_format(AVCodecContext *avctx)
 #endif
 #if CONFIG_AV1_VULKAN_HWACCEL
         *fmtp++ = AV_PIX_FMT_VULKAN;
+#endif
+#if CONFIG_AV1_V4L2REQUEST_HWACCEL
+        *fmtp++ = AV_PIX_FMT_DRM_PRIME;
 #endif
         break;
     case AV_PIX_FMT_YUV420P12:
@@ -1629,6 +1636,9 @@ const FFCodec ff_av1_decoder = {
 #endif
 #if CONFIG_AV1_VULKAN_HWACCEL
         HWACCEL_VULKAN(av1),
+#endif
+#if CONFIG_AV1_V4L2REQUEST_HWACCEL
+        HWACCEL_V4L2REQUEST(av1),
 #endif
 
         NULL
